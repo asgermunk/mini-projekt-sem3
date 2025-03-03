6482076ff88a70f1ad04dc9969b0ded7f76d2d84
asgermunk <75023079+asgermunk@users.noreply.github.com>
2025-03-03 09:28:13 +0100

    temp

--- Program.cs
+++ Program.cs
@@ -4,10 +4,11 @@ var app = builder.Build();
 var AllowSomeStuff = "_AllowSomeStuff";
 builder.Services.AddCors(options =>
 {
-options.AddPolicy(name: AllowSomeStuff, builder => {
-builder.AllowAnyOrigin()
-     .AllowAnyHeader()
-     .AllowAnyMethod();
+    options.AddPolicy(name: AllowSomeStuff, builder =>
+    {
+        builder.AllowAnyOrigin()
+             .AllowAnyHeader()
+             .AllowAnyMethod();
     });
 });
 // ...
@@ -15,7 +16,11 @@ app.UseCors(AllowSomeStuff);
 
 
 
-
+// /api/posts/{id}/upvote
+app.MapPost
+// /api/posts/{id}/downvote
+// /api/posts/{postid}/comments/{commentid}/upvote
+// /api/posts/{postid}/comments/{commentid}/downvote
 
 app.MapGet("/", () => "Hello World!");
 
