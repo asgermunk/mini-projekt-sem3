2d75e7e761536cf6e24dde68d3bc8e9961ef56bc
asgermunk <75023079+asgermunk@users.noreply.github.com>
2025-03-04 09:13:49 +0100

    fdg

--- shared/obj/Debug/net9.0/shared.AssemblyInfo.cs
+++ shared/obj/Debug/net9.0/shared.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5046cd672a142abbb4e87e795a56dd5aac44fa72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc9befed19b091218e754f07b1912c510e943404")]
 [assembly: System.Reflection.AssemblyProductAttribute("shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
